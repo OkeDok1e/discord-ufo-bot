0dffd1f93e6a59705a80c0686f7e19b6c459dbb8
OkeDok1e <asolgat@outlook.com>
2025-06-14 14:59:25 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import discord
+import random
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"We are ready to go in, {bot.user.name}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if random.randint(1, 50) == 1:
+        await message.channel.send('\*flies by\*')
+
+    await bot.process_commands(message) 
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
